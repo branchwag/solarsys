1b07084e84bb92afc205e579e172036d86104ddc
branchwag <branchleannelmb@gmail.com>
2024-09-28 20:09:30 -0400

    makes a nice starry background

--- solarsys.c
+++ solarsys.c
@@ -4,6 +4,12 @@
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 800
+#define NUM_STARS 100
+
+typedef struct {
+	int x;
+	int y;
+} Star;
 
 //mebbe struct for planet later
 
@@ -11,26 +17,21 @@
 int main(void) {
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Solar Sys");
 	SetTargetFPS(60);
-	int frameCount = 0;
-	int numOfStars = 100;
 	
+	srand(time(NULL));
+	Star stars[NUM_STARS];
 
-	
+	for (int i = 0; i < NUM_STARS; i++) {
+	  stars[i].x = rand() % SCREEN_WIDTH;
+	  stars[i].y = rand() % SCREEN_HEIGHT;
+	}
 
 	while (!WindowShouldClose()) {
-	  frameCount++;
 	  BeginDrawing();
 	  ClearBackground(BLACK);
-
-	  //draw stars	
-	srand(time(NULL));
-	int randomPosNumber = rand();
-	int rangePosMin = 1;
-	int rangePosMax = SCREEN_WIDTH;
-	int randomPosInRange = (rand() % (rangePosMax - rangePosMin + 1)) + rangePosMin;
-
-	  for (int i = 0; i < numOfStars; i++) {
-	  DrawText(".", randomPosInRange, randomPosInRange, 16, WHITE);
+	  
+	  for (int i = 0; i < NUM_STARS; i++) {
+		DrawText(".", stars[i].x, stars[i].y, 16, WHITE);
 	  }
 	  //draw 3D spheres for planets
 	  EndDrawing();
