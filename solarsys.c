b637db306da3abae8e6e7007a4c7068e532033a4
branchwag <branchleannelmb@gmail.com>
2024-09-28 20:28:22 -0400

    makes stars twinkle

--- solarsys.c
+++ solarsys.c
@@ -1,6 +1,7 @@
 #include "raylib.h"
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 800
@@ -9,10 +10,16 @@
 typedef struct {
 	int x;
 	int y;
+	float brightness;
+	float twinkleSpeed;
+	float twinklePhase;
 } Star;
 
 //mebbe struct for planet later
 
+float RandomFloat(float min, float max) {
+	return min + (float)rand() / (float)RAND_MAX * (max - min);
+}
 
 int main(void) {
 	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Solar Sys");
@@ -23,15 +30,30 @@ int main(void) {
 
 	for (int i = 0; i < NUM_STARS; i++) {
 	  stars[i].x = rand() % SCREEN_WIDTH;
-	  stars[i].y = rand() % SCREEN_HEIGHT;
+	  stars[i].y = rand() % SCREEN_HEIGHT;       
+	  stars[i].brightness = RandomFloat(0.5f, 1.0f);
+          stars[i].twinkleSpeed = RandomFloat(2.0f, 5.0f);
+          stars[i].twinklePhase = RandomFloat(0, 2 * PI);
 	}
 
+	float timeElapsed = 0.0f;
+
 	while (!WindowShouldClose()) {
+	  timeElapsed += GetFrameTime();
+
 	  BeginDrawing();
 	  ClearBackground(BLACK);
 	  
 	  for (int i = 0; i < NUM_STARS; i++) {
-		DrawText(".", stars[i].x, stars[i].y, 16, WHITE);
+            float twinkle = sinf(timeElapsed * stars[i].twinkleSpeed + stars[i].twinklePhase);
+            stars[i].brightness = (twinkle + 1.0f) / 2.0f * 0.5f + 0.5f;
+            
+            unsigned char alpha = (unsigned char)(stars[i].brightness * 255);
+            Color starColor = {255, 255, 255, alpha};
+
+            int starSize = (twinkle > 0) ? 2 : 1;
+
+            DrawCircle(stars[i].x, stars[i].y, starSize, starColor);
 	  }
 	  //draw 3D spheres for planets
 	  EndDrawing();
