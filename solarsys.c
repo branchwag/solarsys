df77d422318d52ccea354fcc20ae1e7ea4842856
branchwag <branchleannelmb@gmail.com>
2024-10-04 18:37:48 -0400

    implemments texture a bit better

--- solarsys.c
+++ solarsys.c
@@ -24,7 +24,7 @@ int main(void) {
 	SetTargetFPS(60);
 
 	Camera3D camera = { 0 };
-	camera.position = (Vector3){ 0.0f, 2.0f, 50.0f };
+	camera.position = (Vector3){ 0.0f, 2.0f, 25.0f };
 	camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };
 	camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
 	camera.fovy = 45.0f;
